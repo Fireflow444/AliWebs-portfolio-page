8cb82e4717480b9e40ade14b2ff7f859159def6f
Fireflow444 <socialmediaframe9@gmail.com>
2026-02-15 11:16:56 -0700

    updated

--- pratices/Js/js7.js
+++ pratices/Js/js7.js
@@ -57,10 +57,88 @@ let narr=arr.reduce((e1,e2)=>{
 });
 console.log(narr)
 }
-ex5()
+// ex5()
+
+
+function namegenrater(){
+let fstr="";
+//declare name
+let first_name={fn1:"crazy",fn2:"amazing",fn3:"fire"};
+let second_name={sn1:"engine",sn2:"foods",sn3:"garments"};
+let third_name={tn1:"bros",tn2:"limited",tn3:"hub"};
+     //random key
+let ran1=Math.random();
+ if(ran1<0.2){
+fstr=`${first_name.fn1}-${second_name.sn3}-${third_name.tn1}`;
+}
+
+else   if(ran1>0.2 && ran1<0.4){
+fstr=`${first_name.fn2}-${second_name.sn3}-${third_name.tn3}`;
+}
+
+  else if(ran1>0.4 && ran1 <0.7){
+fstr=`${first_name.fn3}-${second_name.sn2}-${third_name.tn1}`;
+}
+     
+ else if(ran1>0.7 && ran1<0.9){
+fstr=`${first_name.fn2}-${second_name.sn1}-${third_name.tn3}`;
+}
+console.log(ran1)
 
 
 
 
+console.log(fstr)
+
+}
+// namegenrater()
 
 
+function factorialcall(){
+
+  let str = "";  // declare first
+
+  let evbtn = document.getElementsByTagName("button")[0];
+  let outbox = document.getElementsByTagName("h2")[0];
+  let inpbr = document.getElementsByTagName("input")[0];
+
+  inpbr.addEventListener("keydown", (e) => {
+    let key = e.key;
+
+    if (key === "Backspace") {
+      str = str.slice(0, -1);
+    } 
+    else if (key.length === 1) {
+      str += key;
+    }
+  });
+evbtn.addEventListener("click", () => {
+  outbox.style.opacity="0%";
+  evbtn.style.animation="bani 3s ease-in-out infinite";
+  setTimeout(()=>{
+  console.log(str);
+  let facstr=facto(str);
+  outbox.textContent = facstr;  // phir new value set\
+str="";
+//effects
+outbox.style.opacity="100%";
+    evbtn.style.animation="";
+  },2000);
+});
+
+}
+factorialcall();
+
+function facto(num){
+let re=1;
+if(num==0){
+  re="Undefined";  
+}
+else{
+for(let i=1;i<=num;i++){
+  re=re*i;
+}
+
+}
+return re;
+}
